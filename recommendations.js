dd9b8bf3062e923a44e1298be7ce4d0029baa670
Greg Perkins <gregperkins@alum.mit.edu>
2012-08-04 09:26:46 -0700

    Sizes recommendations based on rank

--- recommendations.js
+++ recommendations.js
@@ -1,6 +1,13 @@
-
 if (Meteor.is_client) {
   Template.recommendations.list = function () {
-    return state.recommendations;
+    var recs = state.recommendations;
+    $.map(recs, function(rec) {
+      // This maps ranks in (-inf, 100] to size [1,3]
+      rec.size = Math.ceil(Math.sqrt(Math.max(rec.rank,0))/3.4);
+
+      rec.width = rec.height = rec.size * 100;
+      return rec;
+    });
+    return recs;
   };
 }
