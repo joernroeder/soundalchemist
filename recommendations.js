79aa465b35a10c18558c5e6707cf49aa8f0543b2
Avital Oliver <avital@thewe.net>
2012-08-04 10:00:00 -0700

    wip

--- recommendations.js
+++ recommendations.js
@@ -1,15 +1,15 @@
 if (Meteor.is_client) {
   Template.recommendations.list = function () {
-    var recs = state.recommendations;
-    $.map(recs, function(rec) {
-      // This maps ranks in (-inf, 100] to size [1,3]
-      rec.size = Math.ceil(Math.sqrt(Math.max(rec.rank,0))/3.4);
-
-      rec.width = rec.height = rec.size * 100;
-      return rec;
-    });
-    return recs;
+    return state.recommendations;
   };
+
+  Template.recommendation.size = function () {
+    // This maps ranks in (-inf, 100] to size [1,3]
+    var size = Math.ceil(Math.sqrt(Math.max(this.rank,0))/3.4);
+    return {width: size * 100, height: size * 100};
+  };
+
+//  Template.recommendation
 }
 
 if (typeof SoundAlchemist == "undefined") SoundAlchemist = {};
