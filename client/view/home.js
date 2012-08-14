b531fdec886c7ac1d91a192a1b0246f709954afb
Yaniv Oliver <yanivoliver@gmail.com>
2012-08-14 22:10:54 +0300

    Improved url entry behaviour

--- client/view/home.js
+++ client/view/home.js
@@ -7,7 +7,8 @@ SoundAlchemist.view.home = function() {
 
   var defaultUrl = Session.get('home:defaultUrl');
   if (defaultUrl) {
-    SoundAlchemist.view.home.maybeRegisterUrl(defaultUrl);
+    Session.set('home:url', defaultUrl);
+    //SoundAlchemist.view.home.registerUrl(defaultUrl);
   }
 };
 
@@ -24,18 +25,26 @@ SoundAlchemist.view.home.startJourney = function() {
   _SA.Router.navigate('at/' + pointId, {trigger: true});
 };
 
-// Validate the url and set it to the session
-SoundAlchemist.view.home.maybeRegisterUrl = function(soundcloudUrl) {
-  // TODO(gregp): url validation -- don't set if invalid
-  Session.set('home:url', soundcloudUrl);
+// Validate the url
+SoundAlchemist.view.home.validateUrl = function(url) {
+  var regex = /^((http:\/\/){0,1})(soundcloud.com|www.soundcloud.com)([0-9a-zA-Z./_-]+)$/gi;
+  var validUrl = regex.test(url);
+  Session.set('home:badUrl', !validUrl);
+  return validUrl;
+};
+
+// Set the url to the session
+SoundAlchemist.view.home.registerUrlAndStartJourney = function() {
   Session.set('home:ready', false);
   Session.set('home:pending', true);
 
+  soundcloudUrl = Session.get('home:url');
+
   // Get a head start on the recommendations
-  // console.log('DEBUG: getting track data from url ', soundcloudUrl);
+  console.log('DEBUG: getting track data from url ', soundcloudUrl);
   getTrackDataFromUrl(soundcloudUrl, function(error, trackId) {
     if (error) {
-      Session.set('home:pending', false);
+      Session.set('home:ready', false);
       Session.set('home:pending', false);
     }
     Session.set('home:trackId', trackId);
@@ -47,11 +56,11 @@ SoundAlchemist.view.home.maybeRegisterUrl = function(soundcloudUrl) {
       // TODO(gregp): can Meteor set multiple session variables at once?
       Session.set('home:pending', false);
       Session.set('home:ready', true);
+
+      SoundAlchemist.view.home.startJourney();
     });
   });
 };
-
-
 
 // TODO(gregp): nobody else keeps this?!...
 keyCodes = {
@@ -60,39 +69,51 @@ keyCodes = {
 };
 
 ////////////////// URL Input Field ////////////////////////
-
+// TODO(yanivoliver): pasting a url causes weird keydown behaviour - fix it.
 SoundAlchemist.view.home.onUrlKeydown = function(ev) {
   var input = $(ev.target);
   // If escape, make sure the user remembers to add the http
   if (ev.keyCode == keyCodes.ESCAPE) {
     input.val(Session.get('home:defaultUrl'));
+    Session.set('home:badUrl', false);
     return;
   }
 
   // Otherwise, run our validation
   var urlVal = input.val();
-  SoundAlchemist.view.home.maybeRegisterUrl(urlVal);
+  if (!SoundAlchemist.view.home.validateUrl(urlVal)) {
+    return;
+  }
+  Session.set('home:url', urlVal);
 
   // Maybe even start them from here
   if (ev.keyCode == keyCodes.ENTER) {
-    SoundAlchemist.view.home.startJourney();
+    SoundAlchemist.view.home.registerUrlAndStartJourney();
   }
 };
 
 // Clear the field so pasting is easier
 SoundAlchemist.view.home.onUrlClick = function(ev) {
   var input = $(ev.target);
-  input.val('');
+  if (input.val() === Session.get('home:defaultUrl')) {
+    input.val('');
+  }
+  SoundAlchemist.view.home.validateUrl(input.val());
 };
 
 SoundAlchemist.view.home.onUrlBlur = function(ev) {
   var input = $(ev.target);
-  input.val(Session.get('home:defaultUrl'));
+  if (input.val() === "") {
+    input.val(Session.get('home:defaultUrl'));
+  }
+  SoundAlchemist.view.home.validateUrl(input.val());
 };
 
 /////////////////// Start Journey Button //////////////////////
 SoundAlchemist.view.home.onStartClick = function(ev) {
-  SoundAlchemist.view.home.startJourney();
+  var input = $(ev.target);
+  debugger;
+  SoundAlchemist.view.home.registerUrlAndStartJourney();
 };
 
 
@@ -107,20 +128,20 @@ Template.home.pending = function() {
 };
 
 Template.home.notReady = function() {
-  return !Session.get('home:ready');
+  return !Session.get('home:ready') || Session.get('home:badUrl');
 };
 
 Template.home.badUrl = function() {
-  return !Session.get('home:ready') && !Session.get('home:pending');
+  return Session.get('home:badUrl');
 };
 
 Template.home.events = {
   'keydown #soundcloud-url': SoundAlchemist.view.home.onUrlKeydown,
   'focusout #soundcloud-url': SoundAlchemist.view.home.onUrlBlur,
   'click #soundcloud-url': SoundAlchemist.view.home.onUrlClick,
-
   'click #start-journey': SoundAlchemist.view.home.onStartClick
 };
 
 Session.set('home:pending', false);
-Session.set('home:ready', false);
+Session.set('home:ready', true);
+Session.set('home:badUrl', false);
