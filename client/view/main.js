e727454e18da2fe4bf7b67ff9754186052652b8c
Jörn Röder <kontakt@joernroeder.de>
2013-01-23 10:37:40 +0100

    added "page"-class to main div

--- client/view/main.js
+++ client/view/main.js
@@ -6,4 +6,8 @@ Template.main.isPoint = function() {
   return Session.get('page') == SoundAlchemist.view.POINT;
 };
 
+Template.main.getClass = function () {
+	return 'is-' + Session.get('page');
+};
+
 Backbone.history.start({pushState: true});
