d672f92f03ba48a2fb5105324944dcca15897d63
Jörn Röder <kontakt@joernroeder.de>
2013-01-24 09:25:52 +0100

    added in- and outwards

---
+++ client/ui/inwards.js
@@ -0,0 +1,22 @@
+
+
+Template.inwards.title = function () {
+	return "Inwards";
+};
+
+Template.inwards.list = function () {
+	var pointId = Session.get("point:id"),
+		point = _SA.Points.findOne({
+			pointId		: pointId
+		}),
+		trail = (point && point.trail) || [],
+		result = [];
+
+	for (var i in trail) {
+		if (trail[i].weight > 0) {
+			result.push(trail[i]);
+		}
+	}	
+
+	return result;
+};
---
+++ client/ui/outwards.js
@@ -0,0 +1,22 @@
+
+
+Template.outwards.title = function () {
+	return "Outwards";
+};
+
+Template.outwards.list = function () {
+	var pointId = Session.get("point:id"),
+		point = _SA.Points.findOne({
+			pointId		: pointId
+		}),
+		trail = (point && point.trail) || [],
+		result = [];
+
+	for (var i in trail) {
+		if (trail[i].weight < 0) {
+			result.push(trail[i]);
+		}
+	}	
+
+	return result;
+};
